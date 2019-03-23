c38b2e2de919d5c4c52bb6bee8c0e1ed98d393b2
itbox.xyz <itbox.xyz@gmail.com>
2019-03-23 23:14:24 +0800

    测试数据库连接文件

---
+++ mysqltest.py
@@ -0,0 +1,12 @@
+import MySQLdb
+
+conn = MySQLdb.connect(
+    host="121.41.8.92",
+    user="*******",
+    passwd="********",
+    db="youyudic",
+    port=3306)
+
+cur = conn.cursor()
+tables = cur.execute("select * from stardict_mini")
+print(cur.fetchall())
